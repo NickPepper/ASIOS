56a0c5c18353801871d4059d2b2313dd880c58bb
Nick Pershin <nickpeppper@gmail.com>
2013-09-29 15:06:48 +0400

    WBXML constants added

---
+++ ASIOS/ASIOS/Constants.h
@@ -0,0 +1,37 @@
+//
+//  Constants.h
+//  ASIOS
+//
+//  Created by  Nick Pershin on 9/29/13.
+//  Copyright (c) 2013  Nick Pershin. All rights reserved.
+//
+
+#ifndef ASIOS_Constants_h
+#define ASIOS_Constants_h
+
+#pragma mark WBXML constants
+
+#define WBXML_SWITCH_PAGE 0x00
+#define WBXML_END         0x01
+#define WBXML_ENTITY      0x02
+#define WBXML_STR_I       0x03
+#define WBXML_LITERAL     0x04
+#define WBXML_EXT_I_0     0x40
+#define WBXML_EXT_I_1     0x41
+#define WBXML_EXT_I_2     0x42
+#define WBXML_PI          0x43
+#define WBXML_LITERAL_C   0x44
+#define WBXML_EXT_T_0     0x80
+#define WBXML_EXT_T_1     0x81
+#define WBXML_EXT_T_2     0x82
+#define WBXML_STR_T       0x83
+#define WBXML_LITERAL_A   0x84
+#define WBXML_EXT_0       0xC0
+#define WBXML_EXT_1       0xC1
+#define WBXML_EXT_2       0xC2
+#define WBXML_OPAQUE      0xC3
+#define WBXML_LITERAL_AC  0xC4
+
+#define WBXML_WITH_CONTENT  0x40 // !!!
+
+#endif
